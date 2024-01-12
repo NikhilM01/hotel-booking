2b3aa99e5fc03dcca7261f36699c93dffa45a98b
NikhilM01 <nikhilmuneshwar01@gmail.com>
2024-01-12 20:03:28 +0530

    component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
+import { UsersComponent } from './pages/users/users.component';
+import { NewBookingComponent } from './pages/new-booking/new-booking.component';
+import { BookingCalenderComponent } from './pages/booking-calender/booking-calender.component';
+import { BookingListComponent } from './pages/booking-list/booking-list.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RoomsComponent,
+    UsersComponent,
+    NewBookingComponent,
+    BookingCalenderComponent,
+    BookingListComponent,
+    CustomerComponent,
+    DashboardComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
